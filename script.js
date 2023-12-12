e0b72bc33949504d56757a031668f216b9accc9c
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-12 22:28:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const daysOfAYear = (year) =>
 	{
-		if((year%4===0 year%100 !== 0) || year%400===0)
+		if((year%4===0 && year%100 !== 0) || (year%400===0))
 		{
             return 366;
 		}
