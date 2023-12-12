16c9e782d5ead635806e5366fdc1b706d44d9a55
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-12 22:18:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const daysOfAYear = () =>
+	{
+		let y = prompt("enter year");
+		if(y%4==0 y%100 != 0)
+		{
+			console.log(366);
+		}
+		else {
+			console.log(365);
+		}
+	}
